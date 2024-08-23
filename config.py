4f58e376f3bc239b9302822d03ad1b837d9a3a24
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-08-23 13:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_ID = os.environ.get("API_ID", "29940662")
     API_HASH = os.environ.get("API_HASH", "b9ccc26a417ce3a998d321086a2edbb6")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6748961187:AAFMdcvheBBL_Yqv6QpwHb11Iw0-Ej2RINA") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "BQHI27YAot-guzWshSGoS3b1SaeJ9Du2VPPYVWO__PzdW5Stbm1SjtBOq6A1OGUYAcSf5jyAQoOAO3ll9djKVODFSFywp9SwyJ16mOdOE0BVSfC4UonXLex_FLeDsbX2kpXXO_c2QFzR3jq39y3vLCnAD7BswPp59Lw-5-6t_BU1yxh1j7X5GN8vVC0oySJj892KpX9p-mNHJt7b827lUEzvifOyAFg8zpocvezcX3Jndtb-c9h4ov9zYudvjzXIJDrxRfC9YopHIJjYjRVmujs_9q1SQ79bzgL7Rb1riJxSFBhVHdwhiXsqN0Ypxj2P6BBYl4ZuX5jj7ig-TnUSoLFgLm54-gAAAAGSRPmjAQ") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://david:surya@cluster12.f7tpy44.mongodb.net/")
     DB_NAME = os.environ.get("DB_NAME", "Stubborn")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6359874284').split()]
