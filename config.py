e726a94c3a939cb1fcc238650b78823f201b13b6
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-08-29 12:01:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "29940662")
     API_HASH = os.environ.get("API_HASH", "b9ccc26a417ce3a998d321086a2edbb6")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6748961187:AAFMdcvheBBL_Yqv6QpwHb11Iw0-Ej2RINA") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6748961187:-Ej2RINA") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://david:surya@cluster12.f7tpy44.mongodb.net/")
     DB_NAME = os.environ.get("DB_NAME", "Stubborn")
